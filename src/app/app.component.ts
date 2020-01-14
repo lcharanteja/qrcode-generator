0b1e2eb74b091119b2c106e6e5155b4c41eea03f
Charan Teja Lellaboyena <charan@Charans-MBP-2.fios-router.home>
2020-01-13 22:16:09 -0500

    added download logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,26 @@ export class AppComponent {
     // assign a value
     this.myAngularxQrCode = 'Your QR code data string';
   }
+
+  qrcodename: string;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
+  value: string;
+  display = false;
+  href: string;
+
+  generateQRCode() {
+    if (this.qrcodename === '') {
+      this.display = false;
+      alert('Please enter the name');
+      return;
+    } else {
+      this.value = this.qrcodename;
+      this.display = true;
+    }
+  }
+
+  downloadImage() {
+    this.href = document.getElementsByTagName('img')[0].src;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { QRCodeModule } from 'angularx-qrcode';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { QRCodeModule } from 'angularx-qrcode';
     AppRoutingModule,
     BrowserAnimationsModule,
     QRCodeModule,
+    NgxQRCodeModule,
     FormsModule
   ],
   providers: [],
